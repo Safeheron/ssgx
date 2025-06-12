f7ceca76de53be8399982e551d5ccfe2b45e29d8
Max <hejh1500@gmail.com>
2025-06-12 10:51:22 +0800

    Ported mbedtls v3.6.3 to SGX TEE.

--- external/mbedtls/build_and_install_mbedtls_SGX.sh
+++ external/mbedtls/build_and_install_mbedtls_SGX.sh
@@ -6,11 +6,10 @@ set -e
 # Configuration variables
 TOP_DIR="$(cd "$(dirname "$(realpath "${0}")")" && pwd)"
 REPO_URL="https://github.com/Safeheron/mbedtls-SGX.git"  # URL of the mbedtls-SGX repository
-PATCH_FILE="${TOP_DIR}/sgx_mbedtls.patch"  # Path to the patch file (replace with the actual path)
 CLONE_DIR="${TOP_DIR}/mbedtls-SGX"  # Absolute path for cloning the repository
 BUILD_DIR="${CLONE_DIR}/build"  # Absolute path for the build directory
 INSTALL_PREFIX="${INSTALL_PREFIX:-/opt/safeheron/ssgx}"  # Target installation directory, default to /opt/safeheron/ssgx
-TARGET_TAG="eab8e36a1e670a2fa66105735143eafa51931bff"  # commit in branch 'main'
+TARGET_TAG="0ed3bbf2b07a487b0c99fca6abcfd5ff020b92a4"  # commit in branch 'master', tag 'v3.6.3_sgx_1'
 
 # Clone the mbedtls-SGX repository
 if [ -d "${CLONE_DIR}" ]; then
@@ -27,15 +26,6 @@ cd "${CLONE_DIR}"
 # Checkout the specified tag
 echo "Checking out tag '${TARGET_TAG}'..."
 git checkout "${TARGET_TAG}"
-
-# Apply the patch file
-if [ -f "${PATCH_FILE}" ]; then
-    echo "Applying patch: ${PATCH_FILE}..."
-    git apply "${PATCH_FILE}"
-else
-    echo "Error: Patch file '${PATCH_FILE}' not found."
-    exit 1
-fi
 
 # Create and navigate to the build directory
 echo "Creating build directory at '${BUILD_DIR}'..."
